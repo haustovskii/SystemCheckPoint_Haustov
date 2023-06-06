b4b2205f66306e666311c72b5be2d326c3f179fd
haustovskii <haus-tov@mail.ru>
2023-06-06 13:26:05 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using SystemCheckPoint.AppData;
+using SystemCheckPoint.Page;
+
+namespace SystemCheckPoint
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AppFrame.FrameMain = FrameMain;
+            FrameMain.Navigate(new PageLogin());
+        }
+        private void BtnClose_Click(object sender, RoutedEventArgs e) => Close();
+        private void BtnPollUp_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userDb = AppConnect.modelOdb.Employee.FirstOrDefault(x => x.Login == TbxLogin.Text && x.Password == PsbPassword.Password);
+                if (userDb != null)
+                {
+                    MessageBox.Show($"Добро пожаловать {userDb.FirstName} {userDb.Patronumic}", "Успешный вход", MessageBoxButton.OK, MessageBoxImage.Information);
+                    FrameMain.Visibility = Visibility.Visible;
+                    //AppFrame.FrameMain.Navigate(new PageMenuUser(userDb.IDPost));
+                }
+                else
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла критическая ошибка приложения" + ex.ToString(), "Критическая ошибка приложения", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ Page/PageLogin.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using SystemCheckPoint.AppData;
+
+namespace SystemCheckPoint.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : System.Windows.Controls.Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Метод для авторизации пользователя в систему
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userDb = AppConnect.modelOdb.Employee.FirstOrDefault(x => x.Login == TbxLogin.Text && x.Password == PsbPassword.Password);
+                if (userDb != null)
+                {
+                    switch (userDb.IDPost)
+                    {
+                        case 1:
+                            MessageBox.Show($"Добро пожаловать {userDb.FirstName} {userDb.Patronumic}", "Успешный вход", MessageBoxButton.OK, MessageBoxImage.Information);
+                            AppFrame.FrameMain.Navigate(new PageMenuSecurity()); break; //Администратор
+                        case 2:
+                            MessageBox.Show($"Добро пожаловать {userDb.FirstName} {userDb.Patronumic}", "Успешный вход", MessageBoxButton.OK, MessageBoxImage.Information);
+                            AppFrame.FrameMain.Navigate(new PageMenuAdmin()); break; //Охранник
+                        default: MessageBox.Show("Авторизация возможна только для сотрудников КПП и Администрации"); break;
+                    }
+                }
+                else
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла критическая ошибка приложения" + ex.ToString(), "Критическая ошибка приложения", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
